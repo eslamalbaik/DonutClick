a9f32d2f7545e8da5ca8a18902787dc5f2dd1754
Eslam AlBaik <89574402+eslamD7@users.noreply.github.com>
2023-06-12 23:52:28 +0300

    commint-test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,11 @@ module com.example.donutClick {
 
     opens com.example.donutClick to javafx.fxml;
     exports com.example.donutClick;
+}module com.example.donutClick {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.donutClick to javafx.fxml;
+    exports com.example.donutClick;
 }
